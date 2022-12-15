3b37d590f75fac8710445c469733ae749572ca78
CtripleU <cuoyemike@gmail.com>
2022-12-15 23:27:29 +0200

    fixes

--- web_stack_debugging_4/1-user_limit.pp
+++ web_stack_debugging_4/1-user_limit.pp
@@ -2,6 +2,11 @@
 
 exec { 'change user limits':
   provider => shell,
-  command  => 'sed -i -e "s/nofile 5/nofile 50000/" /etc/security/limits.conf';\
-  sed -i -e "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
+  command  => 'sed -i -e "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
+  before   => exec['change'],
+}
+
+exec {'change':
+  provider => shell,
+  command  => 'sed -i -e "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
 }
